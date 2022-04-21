5cd6a922a3f6413cc1b2ed987ea26708311569ac
Jiahaou <74173951+Jiahaou@users.noreply.github.com>
2022-04-21 10:34:48 +0800

    Update sort.py

--- sort.py
+++ sort.py
@@ -53,11 +53,12 @@ def sum_train(text):#把一个train里面所有的相加，我这把traindat和t
         while True:
             lines=a.readline()
             t+=1
-            if "test_dat" in lines:
+            if "train_dat" in lines:
                 name = lines[17:-3]
+
                 dict[name]=0.0
             else:
-                if "train_dat" in lines:
+                if "test_dat" in lines:
 
                     dict[name]+=(float(next(a)[14:-1]))
 
@@ -78,16 +79,80 @@ def sum_train(text):#把一个train里面所有的相加，我这把traindat和t
         k2=sorted(dict.items(),key=lambda x:x[1],reverse=False)[:ceil((topk/100)*len(dict))]
         w.write(str(k2))
         print(len(k2))
-def spearman(x, y):
-    assert len(x) == len(y) > 0
-    q = lambda n: map(lambda val: sorted(n).index(val) + 1, n)
-    d = sum(map(lambda x, y: (x - y) ** 2, q(x), q(y)))
-    return 1.0 - 6.0 * d / float(len(x) * (len(y) ** 2 - 1.0))
+
 # sum_a("result_Tracin/Tracin-317-macnn_2022-04-08-17-52-37.json")
-# x=[]
-# y=[]
-# sort_a("result_Tracin/Tracin-317-macnn_2022-04-08-17-52-37.json",x)
-# sort_a("result_Tracin/Tracin-317-macnn.json",y)
-# print('Spearman Rho: %f' % spearman(x, y))
-sum_train("result_Tracin/Tracin-1425-macnn_2022-04-13-10-44-40.json")
+
+
+# sum_train("result_Tracin/Tracin-1425-macnn_2022-04-13-10-44-40.json")
 # sum_a("result_Tracin/Tracin-317-macnn_2022-04-08-17-52-37.json")
+
+def sum_checkpoint(text1,text2,text3):  # 把一个train里面所有的相加，我这把traindat和testdat写反了
+    length1 = 0
+    length2 = 0
+    length3 = 0
+    dict = {}
+    with open(text1) as a1:
+        length1 += len(a1.readlines())
+    with open(text2) as a2:
+        length2 += len(a2.readlines())
+    with open(text3) as a3:
+        length3 += len(a3.readlines())
+    with open(text1) as a:
+        t = 0
+        while True:
+            lines = a.readline()
+            t += 1
+            if "train_dat" in lines:
+                name = lines[17:-3]
+                dict[name] = 0.0
+            else:
+                if "test_dat" in lines:
+                    dict[name] += (float(next(a)[14:-1]))
+                if t == length1:
+                    print("finish")
+                    break
+                else:
+                    continue
+    with open(text2) as b:
+        t = 0
+        while True:
+            lines = b.readline()
+            t += 1
+            if "train_dat" in lines:
+                name = lines[17:-3]
+
+            else:
+                if "test_dat" in lines:
+                    dict[name] += (float(next(b)[14:-1]))
+                if t == length2:
+                    print("finish")
+                    break
+                else:
+                    continue
+    with open(text3) as c:
+        t = 0
+        while True:
+            lines = c.readline()
+            t += 1
+            if "train_dat" in lines:
+                name = lines[17:-3]
+
+            else:
+                if "test_dat" in lines:
+                    dict[name] += (float(next(c)[14:-1]))
+                if t == length3:
+                    print("finish")
+                    break
+                else:
+                    continue
+    topk = 70
+    tt = str(os.path.basename(text1).split('-')[1])
+    with open("save_top_no_model/save_top{}_{}_new.json".format(tt, topk), "w") as w:
+        k = sorted(dict.items(), key=lambda x: x[1], reverse=True)[:ceil((topk / 100) * len(dict))]
+        w.write(str(k))
+        print(len(k))
+    with open("save_top_no_model/save_top{}_{}_new_reverse.json".format(tt, topk), "w") as w:
+        k2 = sorted(dict.items(), key=lambda x: x[1], reverse=False)[:ceil((topk / 100) * len(dict))]
+        w.write(str(k2))
+        print(len(k2))
+sum_checkpoint("result_Tracin/Tracin-1425-macnn.json","result_Tracin/Tracin-1425-macnn_2022-04-20-14-31-42.json","result_Tracin/Tracin-1425-macnn_2022-04-20-17-06-39.json")
